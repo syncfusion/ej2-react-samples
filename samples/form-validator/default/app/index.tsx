568d6c42433ede5077b353e7696a5f7125ccd251
pipeline <pipeline@syncfusion.com>
2017-11-08 13:59:38 +0000

    Ej2 5149 label issue master

--- samples/form-validator/default/app/index.tsx
+++ samples/form-validator/default/app/index.tsx
@@ -43,80 +43,125 @@ export class Default extends SampleBase<{}, {}>{
         <div className='control-section'>
           <div className='content-wrapper' style={{ marginBottom: '25px' }}>
             <form id='htmlFormId' className='htmlForm-horizontal'>
-              <div className='e-float-input'>
-                <input type='text' id='required' name='Required' required />
-                <span className='e-float-line'></span>
-                <label className='e-float-text' htmlFor='required'>Required</label>
+              <div className='form-group'>
+                <div className='e-float-input'>
+                  <input type='text' id='required' name='Required' data-msg-containerid='requiredError' />
+                  <span className='e-float-line'></span>
+                  <label className='e-float-text' htmlFor='required'>Required</label>
+                </div>
+                <div id='requiredError'></div>
               </div>
-              <div className='e-float-input'>
-                <input type='text' id='email' name='Email' required />
-                <span className='e-float-line'></span>
-                <label className='e-float-text' htmlFor='email'>Email</label>
+              <div className='form-group'>
+                <div className='e-float-input'>
+                  <input type='text' id='email' name='Email' data-msg-containerid='emailError' />
+                  <span className='e-float-line'></span>
+                  <label className='e-float-text' htmlFor='email'>Email</label>
+                </div>
+                <div id='emailError'></div>
               </div>
-              <div className='e-float-input'>
-                <input type='text' id='url' name='Url' required />
-                <span className='e-float-line'></span>
-                <label className='e-float-text' htmlFor='url'>URL</label>
+              <div className='form-group'>
+                <div className='e-float-input'>
+                  <input type='text' id='url' name='Url' data-msg-containerid='urlError' />
+                  <span className='e-float-line'></span>
+                  <label className='e-float-text' htmlFor='url'>URL</label>
+                </div>
+                <div id='urlError'></div>
               </div>
-              <div className='e-float-input'>
-                <input type='text' id='date' name='Date' required />
-                <span className='e-float-line'></span>
-                <label className='e-float-text' htmlFor='date'>Date</label>
+              <div className='form-group'>
+                <div className='e-float-input'>
+                  <input type='text' id='date' name='Date' data-msg-containerid='dateError' />
+                  <span className='e-float-line'></span>
+                  <label className='e-float-text' htmlFor='date'>Date</label>
+                </div>
+                <div id='dateError'></div>
               </div>
-              <div className='e-float-input'>
-                <input type='text' id='dateIso' name='DateISO' required />
-                <span className='e-float-line'></span>
-                <label className='e-float-text' htmlFor='dateIso'>Date ISO (YYYY-MM-DD)</label>
+              <div className='form-group'>
+                <div className='e-float-input'>
+                  <input type='text' id='dateIso' name='DateISO' data-msg-containerid='dateisoError' />
+                  <span className='e-float-line'></span>
+                  <label className='e-float-text' htmlFor='dateIso'>Date ISO (YYYY-MM-DD)</label>
+                </div>
+                <div id='dateisoError'></div>
               </div>
-              <div className='e-float-input'>
-                <input type='text' id='number' name='Number' required />
-                <span className='e-float-line'></span>
-                <label className='e-float-text' htmlFor='number'>Integer or Decimal</label>
+              <div className='form-group'>
+                <div className='e-float-input'>
+                  <input type='text' id='number' name='Number' data-msg-containerid='numberError' />
+                  <span className='e-float-line'></span>
+                  <label className='e-float-text' htmlFor='number'>Integer or Decimal</label>
+                </div>
+                <div id='numberError'></div>
               </div>
-              <div className='e-float-input'>
-                <input type='text' id='digits' name='Digits' required />
-                <span className='e-float-line'></span>
-                <label className='e-float-text' htmlFor='digits'>Positive Integer</label>
+              <div className='form-group'>
+                <div className='e-float-input'>
+                  <input type='text' id='digits' name='Digits' data-msg-containerid='digitError' />
+                  <span className='e-float-line'></span>
+                  <label className='e-float-text' htmlFor='digits'>Positive Integer</label>
+                </div>
+                <div id='digitError'></div>
               </div>
-              <div className='e-float-input'>
-                <input type='text' id='maxlen' name='MaxLength' required />
-                <span className='e-float-line'></span>
-                <label className='e-float-text' htmlFor='maxlen'>Maximum 5 characters</label>
+              <div className='form-group'>
+                <div className='e-float-input'>
+                  <input type='text' id='maxlen' name='MaxLength' data-msg-containerid='maxlenError' />
+                  <span className='e-float-line'></span>
+                  <label className='e-float-text' htmlFor='maxlen'>Maximum 5 characters</label>
+                </div>
+                <div id='maxlenError'></div>
               </div>
-              <div className='e-float-input'>
-                <input type='text' id='minlen' name='MinLength' required />
-                <span className='e-float-line'></span>
-                <label className='e-float-text' htmlFor='minlen'>Minimum 5 characters</label>
+              <div className='form-group'>
+                <div className='e-float-input'>
+                  <input type='text' id='minlen' name='MinLength' data-msg-containerid='minlenError' />
+                  <span className='e-float-line'></span>
+                  <label className='e-float-text' htmlFor='minlen'>Minimum 5 characters</label>
+                </div>
+                <div id='minlenError'></div>
               </div>
-              <div className='e-float-input'>
-                <input type='text' id='rangelen' name='RangeLength' required />
-                <span className='e-float-line'></span>
-                <label className='e-float-text' htmlFor='rangelen'>Characters length between 5 to 10</label>
+              <div className='form-group'>
+                <div className='e-float-input'>
+                  <input type='text' id='rangelen' name='RangeLength' data-msg-containerid='rangelenError' />
+                  <span className='e-float-line'></span>
+                  <label className='e-float-text' htmlFor='rangelen'>Characters length between 5 to 10</label>
+                </div>
+                <div id='rangelenError'></div>
               </div>
-              <div className='e-float-input'>
-                <input type='text' id='range' name='Range' required />
-                <span className='e-float-line'></span>
-                <label className='e-float-text' htmlFor='range'>Value between 5 to 10</label>
+              <div className='form-group'>
+                <div className='e-float-input'>
+                  <input type='text' id='range' name='Range' data-msg-containerid='rangeError' />
+                  <span className='e-float-line'></span>
+                  <label className='e-float-text' htmlFor='range'>Value between 5 to 10</label>
+                </div>
+                <div id='rangeError'></div>
               </div>
-              <div className='e-float-input'>
-                <input type='text' id='max' name='Max' required />
-                <span className='e-float-line'></span>
-                <label className='e-float-text' htmlFor='max'>Max (maximum value 5)</label>
+              <div className='form-group'>
+                <div className='e-float-input'>
+                  <input type='text' id='max' name='Max' data-msg-containerid='maxError' />
+                  <span className='e-float-line'></span>
+                  <label className='e-float-text' htmlFor='max'>Max (maximum value 5)</label>
+                </div>
+                <div id='maxError'></div>
               </div>
-              <div className='e-float-input'>
-                <input type='text' id='min' name='Min' required />
-                <span className='e-float-line'></span>
-                <label className='e-float-text' htmlFor='min'>Min (minimum value 5)</label>
+              <div className='form-group'>
+                <div className='e-float-input'>
+                  <input type='text' id='min' name='Min' data-msg-containerid='minError' />
+                  <span className='e-float-line'></span>
+                  <label className='e-float-text' htmlFor='min'>Min (minimum value 5)</label>
+                </div>
+                <div id='minError'></div>
               </div>
-              <div className='e-float-input'>
-                <input type='text' id='regex' name='Regex' required />
-                <span className='e-float-line'></span>
-                <label className='e-float-text' htmlFor='regex'>Regex (accepts alphabets only)</label>
+              <div className='form-group'>
+                <div className='e-float-input'>
+                  <input type='text' id='regex' name='Regex' data-msg-containerid='regexError' />
+                  <span className='e-float-line'></span>
+                  <label className='e-float-text' htmlFor='regex'>Regex (accepts alphabets only)</label>
+                </div>
+                <div id="regexError"></div>
               </div>
-              <div className='e-float-input'>
-                <input type='text' id='custom' name='Custom' required />
-                <span className='e-float-line'></span>
-                <label className='e-float-text' htmlFor='custom'>Custom Function (maximum 5 characters)</label>
+              <div className='form-group'>
+                <div className='e-float-input'>
+                  <input type='text' id='custom' name='Custom' data-msg-containerid='customError' />
+                  <span className='e-float-line'></span>
+                  <label className='e-float-text' htmlFor='custom'>Custom Function (maximum 5 characters)</label>
+                </div>
+                <div id='customError'></div>
               </div>
               <div className='row'>
                 <div style={{ float: 'left', margin: '0 10% 0 30%' }}>
