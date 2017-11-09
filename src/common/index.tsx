d7c73690c2afbf3556106383c973d666cfb2b145
pipeline <pipeline@syncfusion.com>
2017-11-09 07:35:04 +0000

    bug(EJ2-5486): Fixed tooltip alignment issue

--- src/common/index.tsx
+++ src/common/index.tsx
@@ -634,6 +634,13 @@ function processResize(e: any): void {
  * Theme Loading
  */
 function loadTheme(theme: string): void {
+  let body: HTMLElement = document.body;
+  if (body.classList.length > 0) {
+      for (let themeItem of themeCollection) {
+        body.classList.remove(themeItem);
+      }
+  }
+  body.classList.add(theme);
   themeList.querySelector('.active').classList.remove('active');
   themeList.querySelector('#' + theme).classList.add('active');
   let ajax: Ajax = new Ajax('./styles/' + theme + '.css', 'GET', true);
